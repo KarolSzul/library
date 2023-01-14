5959a2b96810800cfc5ce8425df05a762c46b3c8
Karol Szul <szul.karol94@gmail.com>
2023-01-14 10:36:24 +0100

    second config

--- src/main/java/library/Main.java
+++ src/main/java/library/Main.java
@@ -1,7 +1,10 @@
 package library;
 
+import library.model.User;
+
 public class Main {
     public static void main(String[] args) {
+        User user = new User(1, "login", "pass", "abc");
         String testCheckstyle = "tekst";
         System.out.println(testCheckstyle);
     }
