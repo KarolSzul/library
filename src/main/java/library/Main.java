a22f430b5968c648aad773863004afcf3001eb60
Karol Szul <szul.karol94@gmail.com>
2023-01-14 10:09:39 +0100

    first config

---
+++ src/main/java/library/Main.java
@@ -0,0 +1,8 @@
+package library;
+
+public class Main {
+    public static void main(String[] args) {
+        String testCheckstyle = "tekst";
+        System.out.println(testCheckstyle);
+    }
+}
